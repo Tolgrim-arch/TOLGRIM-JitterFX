82d1e212d2aad666e2985efcd14a1a77e281d513
Tolgrimu <Tolgrim-arch@users.noreply.github.com>
2026-09-12 16:14:41 -0300

    Convert to Progressive Web App (PWA) with generated icon

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'jitterfx-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './gif.js',
+    './gif.worker.js',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request)
+        .then(response => response || fetch(e.request))
+    );
+});
